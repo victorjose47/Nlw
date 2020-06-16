e901be434345997c22e33c18e16639c0b1f47301
Victor José <vitinhoo_48@hotmail.com>
2020-06-16 02:54:44 -0300

    Último dia finalizado!

--- Nlw/public/scripts/create-point.js
+++ Nlw/public/scripts/create-point.js
@@ -1,65 +1,65 @@
-function populateufs(){
+function populateufs() {
   const ufSelect = document.querySelector("select[name=uf]")
   //Promessa                                                           
   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-  //Outra Promessa
-  .then(res => res.json())
-  .then(states => {
+    //Outra Promessa
+    .then(res => res.json())
+    .then(states => {
 
-    for(const state of states ){
-     
-      ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+      for (const state of states) {
 
-    }
-    
-  })
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+
+      }
+
+    })
 }
 //Executa o evento para preencher os UF's
 populateufs()
 
-function getCities(event){
+function getCities(event) {
   const citySelect = document.querySelector("select[name=city]")
   const stateInput = document.querySelector("input[name=state]")
 
 
-  const ufValue =  event.target.value
+  const ufValue = event.target.value
 
   const indexOfSelectedState = event.target.selectedIndex
   stateInput.value = event.target.options[indexOfSelectedState].text
 
-  const url=`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
   citySelect.innerHTML = '<option value="">Selecione o estado</option>'
   citySelect.disabled = true
 
   fetch(url)
-  .then(res => res.json())
-  .then(cities =>{
-    for(const city of cities){
-      
-      citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>` 
-    }
-    citySelect.disabled = false
-  })
+    .then(res => res.json())
+    .then(cities => {
+      for (const city of cities) {
+
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+      }
+      citySelect.disabled = false
+    })
 
 
 }
 
 document.querySelector("select[name=uf]")
-    .addEventListener("change", getCities);
+  .addEventListener("change", getCities);
 
 //ITENS DE COLETA
 const itemsToCollect = document.querySelectorAll(".items-grid li")
 
-for(const item of itemsToCollect){
+for (const item of itemsToCollect) {
   item.addEventListener("click", handleSelectedItem)
 }
 
-const collectedItems = document.querySelector("input[nome=items]")
+const collectedItems = document.querySelector("input[name=items]")
 
 let selectedItems = []
 
-function handleSelectedItem(event){
+function handleSelectedItem(event) {
 
   const itemLi = event.target
 
@@ -68,30 +68,27 @@ function handleSelectedItem(event){
 
   const itemId = itemLi.dataset.id
 
-  
+console.log("ITEM ID: ", itemId)
 
   //verificar se tem item selecionado se sim
-
   //Pegar os items selecionados
   const alredySelected = selectedItems.findIndex(item => item == itemId)
 
   //Se já estiver selecionado, tirar da selecão
-  if(alredySelected >= 0){
-    filteredItems = selectedItems.filter(item => { 
+  if (alredySelected >= 0) {
+    filteredItems = selectedItems.filter(item => {
       const itemIsDifferent = item != itemId //false
       return itemIsDifferent
     })
-    selectedItems = filteredItems    
-  }else{
+    selectedItems = filteredItems
+  } else {
     //se não tiver selecionado, adicioanar
     selectedItems.push(itemId)
   }
 
-  console.log(selectedItems)
-
-  
+  console.log("selectedItems: ", selectedItems)
 
   //Atualizar o campo escondido com os items selecionados
-  collectedItems.value = selectedItems 
+  collectedItems.value = selectedItems
 
 }
--- Nlw/src/server.js
+++ Nlw/src/server.js
@@ -1,11 +1,17 @@
 const express = require("express")
 const server = express()
-''
+
+//Pega o banco de dados
+const db = require("./database/db")
+
 //Configurar pasta publica
 server.use(express.static("public"))
 
+// Habilitar o usu do REQ BODY
+server.use(express.urlencoded({ extended: true }))
+
 //Ultilizando tamplate engine(permite criar fazer is e else, criar várias que serão enviada de nosso Back-end)
-const nunjucks = require("nunjucks") 
+const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
   express: server,
   noCache: true
@@ -19,11 +25,74 @@ server.get("/", (req, res) => { //REQ(requisição) RES(resposta)
 })
 
 server.get("/create-point", (req, res) => { //REQ(requisição) RES(resposta)
+
+  // req.queryQuery String da nossa url
+  //console.log(req.query)
+
   return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+
+  console.log("BODEYYYYYYYY : ",req.body)
+
+  // Inserir dados no banco de dados
+  //Inserir dados na tabela
+  const query = `
+INSERT INTO places (image, name, address, address2, state, city, items) 
+VALUES (?, ?, ?, ?, ?, ?, ?);`
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+
+  function afterInsertData(err) {
+    if (err) {
+      console.log(err)
+      return res.send("Erro no cadastro!")
+    }
+    console.log('Cadastrado com sucesso!')
+    console.log(this)
+
+    return res.render("create-point.html", {saved: true})
+  }
+
+  db.run(query, values, afterInsertData) // se coloca a function com () ela é executada na hora */
+
+
+
+})
+
 server.get("/search", (req, res) => { //REQ(requisição) RES(resposta)
-  return res.render("search-results.html")
+  
+  const search = req.query.search
+    
+    if(search == ""){
+      //Pesquisa Vázia
+    return res.render("search-results.html", {total: 0 })
+    }
+
+
+  
+  //Pegar os dados do banco de dados
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+    if (err) {
+      return console.log(err)
+    }
+    const total = rows.length
+
+    //Mostrar a página html com os dados do Banco
+    return res.render("search-results.html", { places: rows, total: total })
+
+  })
+
+
 })
 
 //Ligar o servidor
